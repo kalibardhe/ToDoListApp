45a3607bac1f28a8ea1056af9a1fa5657eb4f7a0
Paulo Cavalini <55258447+kalibardhe@users.noreply.github.com>
2023-08-22 14:53:40 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+let newItems = [];
+app.get("/", (req, res) => {
+    let options = { 
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+        };
+        let today = new Date();
+        let day = today.toLocaleDateString("en-US", options);
+    res.render("partials/list", {kindOfDay: day, newListItems: newItems} );
+});
+
+app.post('/', (req, res) => {
+    let newItem = req.body.newItem;
+    newItems.push(newItem);
+    res.redirect('/');
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
